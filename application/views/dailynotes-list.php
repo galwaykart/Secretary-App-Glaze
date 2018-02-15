0783e4a9187aa2a63285bce898ee8c9684906723
surender singh <surender@GLAZE.COM>
2018-02-15 18:45:59 +0530

    Commited for new pages

---
+++ application/views/dailynotes-list.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+ <?php $this->load->view('head'); ?>
+<body class="body style-6"><!-- body tag start -->
+   <!-- globle header for comman start --> 
+    <?php $this->load->view('header'); ?>
+    <!-- user View design page start -->
+    <div class="col-md-12 heading-tag"><p><span class="fa fa-home" ></span>&nbsp;Home / Dailly Notes</p></div>
+    <div class="dailly-notes-index-panel">
+            <div class="table-res">
+                    <div class="table-view">
+                        <table>
+                            <thead>
+                                <tr><th>Sr No</th><th>Date of Entry</th><th>Task</th><th>Task Type</th><th>Target Date</th><th>Department</th><th>Delegate To</th><th>Status</th><th>Remark</th><th>Data Send to other sheet</th></tr>
+                            </thead>
+                            <tbody>
+                                <tr><td>1</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                                <tr><td>2</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                                <tr><td>3</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                                <tr><td>4</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                                <tr><td>5</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                                <tr><td>6</td><td>12/04/2018</td><td>xyz</td><td>Progess</td><td>20/04/2018</td><td>IT Software</td><td>xyz</td><td>Active</td><td>no remarks</td><td>NO</td></tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+            </div>
+    </div><!-- dailly index panel end --> 
+	
+     <br /><br /><br />
+    <div style="height:50px;"></div>
+      <?php $this->load->view('head'); ?>
+</body>
+</html>
