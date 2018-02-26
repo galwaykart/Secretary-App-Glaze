b9b6ef37e81f8436e9ea2c3b4c660e6850ca8e7c
surender singh <surender@GLAZE.COM>
2018-02-26 10:14:23 +0530

    Commited by surender for new pages

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,2 @@
-  
-$('.popup .header #close-popup').on('click', function() {
-    $('.popup').addClass('animation-out').hide();
-});
-$('#addnotes').on('click', function() {
-    $('.popup').show();
-});
+$('.popup .header #close-popup').on('click',function(){$('.popup').addClass('animation-out').hide();});
+$('#addnotes').on('click',function(){$('.popup').show();});
