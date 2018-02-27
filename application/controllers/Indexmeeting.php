546fbed2e0c9c90c1a8103734f2e1cd0d4d54ab7
surender singh <surender@GLAZE.COM>
2018-02-27 15:47:54 +0530

    commite by surender

--- application/controllers/Indexmeeting.php
+++ application/controllers/Indexmeeting.php
@@ -88,6 +88,8 @@
 		 'delegated_dept'=>$this->input->post('delegated_dept'),
 		 'delegated_name'=>$this->input->post('delegated_name'),
 		  );
+		  
+		  print_r($data[2]); die;
 		$data[3]=array(
 		  'agenda_name'=>$this->input->post('agenda')
 		  );
