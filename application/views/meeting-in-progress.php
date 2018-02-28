2ddf9d9396db4b283c8a6fb9efd0bf2573470d84
surender singh <surender@GLAZE.COM>
2018-02-28 13:47:07 +0530

    commite fir index view pages

--- application/views/meeting-in-progress.php
+++ application/views/meeting-in-progress.php
@@ -8,24 +8,37 @@
     <div class="col-md-12 heading-tag"><p><span class="fa fa-home" ></span>&nbsp;Home / Users</p></div>
     <div class="col-md-12 all-mettings">
             <div class="container">
-                <h2 class="heading">All Mettings</h2>
+                <h2 class="heading">All Meeting</h2>
             </div>
         <div class="Mettings-View-index-panel">
              
                <div class="table-res">
                    <table id="mettings-table">
                        <thead>
-                           <tr><th>Sno</th><th>Ajenda</th><th>No of Mettings</th><th>Previous Meetings Date</th><th>Next Metting Date</th><th>Participants</th><th>Metting Called By (Name & Department)</th><th>Remarks</th><th>Active/Inactive</th></tr>
+                           <tr>
+							   <th>Sno</th><th>Ajenda</th>
+							   <th>No of Meeting</th>
+							   <th>Previous Meetings Date</th>
+							   <th>Next Meeting Date</th>
+							   <th>Participants</th>
+							   <th>Meeting Called By (Name & Department)</th>
+							   <th>Remarks</th>
+							   <th>Active/Inactive</th>
+						   </tr>
                        </thead>
                        <tbody>
-					   <?php 
-					   $i=1;
-					   foreach($meeting as $arr){?>
-                        <?php echo "<tr onclick='newDoc($arr->agenda_id)'>"?>
-						 <td><?php echo $i++;?></td>
-						 <td><?php echo $arr->agenda_name;?></td>
-						 <td><?php echo $arr->counter;?></td>
-
+					   <?php  $i=1;
+					     foreach($meeting as $arr){?>
+                         <tr onclick='newDoc(<?php echo $arr->agenda_id; ?>)'>
+							 <td><?php echo $i++;?></td>
+							 <td><?php echo $arr->agenda_name;?></td>
+							 <td><?php echo $arr->counter;?></td>
+							 <td></td>
+							 <td></td>
+							 <td></td>
+							 <td></td>
+							 <td></td>
+							 <td></td>
                          </tr>
 					   <?php } ?>
                        </tbody>
