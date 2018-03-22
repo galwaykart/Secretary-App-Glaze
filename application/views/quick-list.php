2fb8c934bbf9cc4a9637dd5da9e142f4411fd961
developer0705 <gaurav.gupta@glazegalway.com>
2018-03-22 17:37:52 +0530

    worked on dashboard

--- application/views/quick-list.php
+++ application/views/quick-list.php
@@ -61,6 +61,7 @@
       
 
 </script>
+
         <div class="clear"></div>
         <div class="col-md-12">
             <br />
--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
 // $('.popup .header #close-popup').on('click',function(){$('.popup').addClass('animation-out').hide();});
 // $('#addnotes').on('click',function(){$('.popup').show();});
 
 
 
 $('.popup .header #close-popup').on('click',function(){
-=======
- // $('.popup .header #close-popup').on('click',function(){$('.popup').addClass('animation-out').hide();});
- // $('#addnotes').on('click',function(){$('.popup').show();});
-
- 
- 	$('.popup .header #close-popup').on('click',function(){
->>>>>>> 6b4fe13e3629c52311dd8262cc009cb8e9fccc46
     $('.popup').addClass('animation-out').hide();
     $('#myForm input[type="text"]').val('');
     $('#myForm input[type="date"]').val('');
     $('#myForm textarea').val('');
-<<<<<<< HEAD
-=======
-    $('#myForm input[type="Time"]').val('');
->>>>>>> 6b4fe13e3629c52311dd8262cc009cb8e9fccc46
     $('#myForm select').val('');
     $('#refresh_popup .auto-del').remove();
 	$('#auto-del').append('<div class="auto-del"><div class="left"><div class="form-group"><label>Delegate To</label><div class="input-group"><div class="form-control"><input required="" type="text" id="gm1" name="delegate_to[]" title="Delegate To"></div></div></div></div><div class="center"><div class="form-group"><label>Email Id</label><div class="input-group"><div class="form-control"><input required="" type="text" id="gm2" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" name="delegate_email[]"></div></div></div></div><div class="right text-center"><div class="btn-group"><a href="#" class="plus" id="plus"><span class="fa fa-plus"></span></a></div></div></div>');
 });
 
 
-<<<<<<< HEAD
     $('#addnotes').on('click',function(){$('.popup').show();});
-=======
-    $('#addnotes').on('click',function(){$('.popup').show();});
->>>>>>> 6b4fe13e3629c52311dd8262cc009cb8e9fccc46
